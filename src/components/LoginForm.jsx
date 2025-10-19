e192a19407c6140602e1a44a291e349803e61d56
Ewa <ewahjelm84@gmail.com>
2025-10-19 23:41:14 +0200

    LoginForm and Login page created

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,43 @@
+import InputField  from './InputField';
+import Button from './Button';
+import UserFeedback  from './UserFeedback';
+import styles from './LoginForm.module.css';
+
+const LoginForm = ({ 
+  values = { email: '', password: '' },
+  onChange = () => {},
+  onSubmit = () => {},
+  loading = false,
+  error = null,
+}) => {
+  return (
+    <form
+      onSubmit={onSubmit}
+      className={styles.form}
+    >
+      <h2>Logga in</h2>
+
+      <InputField
+        name="username"
+        label="Användarnamn"
+        value={values.username}
+        onChange={onChange}
+      />
+
+      <InputField
+        name="password"
+        label="Lösenord"
+        type="password"
+        value={values.password}
+        onChange={onChange}
+      />
+
+      {error && <UserFeedback feedback={error} />}
+      <Button type="submit" disabled={loading}>
+        {loading ? "Loggar in..." : "Logga in"}
+      </Button>
+    </form>
+ ) ;
+};
+
+export default LoginForm;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,40 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-
+import  LoginForm  from '../components/LoginForm'
+import InputField from '../components/InputField';
+import Button from '../components/Button';
+ 
 const Login = () => {
+    const [values, setValues] = useState({ username: "", password: "" });
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const handleChange = (e) => {
+        console.log(e.target.name, e.target.value);
+        setValues({ ...values, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setLoading(true);
+
+        // Mock delay för test
+        setTimeout(() => {
+        setLoading(false);
+        setError("Felaktig inloggning!");
+        }, 1000);
+    };
+
     return (
         <div>
-           <h3>Logga in</h3>
-           {/* <LoginForm/> */}
+           <h3>Välkommen till Shut-up chat-app</h3>
+            <LoginForm
+                values={values}
+                onChange={handleChange}
+                onSubmit={handleSubmit}
+                loading={loading}
+                error={error}
+            />
            <p>Har du inget konto?</p>
             <Link to="/register">Registrera dig här</Link>
         </div>
